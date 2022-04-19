7436777455d3ab320cce6706974b1bd44a82cc50
Hugo Blom <bl0m1@users.noreply.github.com>
2022-04-19 23:06:44 +0200

    Update entrypoint.sh

--- ansible-lint/entrypoint.sh
+++ ansible-lint/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Running: git config --global --add safe.directory /github/workspace"
+git config --global --add safe.directory /github/workspace
+
 echo "Running: find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint --force-color -vv"
 sh -c "find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint --force-color -vv"
