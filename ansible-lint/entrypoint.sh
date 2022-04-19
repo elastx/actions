669404cbf8ee054ba3199458d0b7c17451c7945e
Hugo Blom <bl0m1@users.noreply.github.com>
2022-04-19 16:11:19 +0200

    Update entrypoint.sh

--- ansible-lint/entrypoint.sh
+++ ansible-lint/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Running: find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint -vv"
-sh -c "find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint -vv"
+echo "Running: find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint --force-color -vv"
+sh -c "find $3 -type f -maxdepth $4 -name '*.yml' | xargs ansible-lint --force-color -vv"
